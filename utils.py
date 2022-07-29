9f6baa3a743160269520437120526c4ba4a3b0b9
nverchev <nicolas.vercheval@gmail.com>
2022-07-29 19:43:30 +0200

    smaller batch

--- utils.py
+++ utils.py
@@ -25,13 +25,12 @@ def knn(x, k):
 
 def get_graph_feature(x, k=20):
 
-    x = x.transpose(2, 1).contiguous()
+    x = x.transpose(2, 1)
     batch, n_points, n_feat = x.size()
     idx = knn(x, k=k)  # (batch_size, num_points, k)
     idx = idx.view(batch, k * n_points, 1).expand(-1, -1,  n_feat)
     feature = torch.gather(x, 1, idx).view(batch, n_points, k, n_feat)
     x_feat = x.unsqueeze(2).expand(-1, -1, k, -1)
-    # important contiguous after permute
     feature = torch.cat((feature - x_feat, x_feat), dim=3).permute(0, 3, 1, 2)
     # (batch_size, num_points, k, 2*num_dims) -> (batch_size, 2*num_dims, num_points, k)
     return feature  # (batch_size, 2*num_dims, num_points, k)
