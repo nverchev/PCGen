890c25d72fa9545559a9e1fe114864e5d6c32f00
nverchev <nicolas.vercheval@gmail.com>
2022-09-04 02:44:28 +0200

    preproc neighbours

--- src/dataset.py
+++ src/dataset.py
@@ -52,10 +52,10 @@ def load_h5(wild_path, num_points, k):
                     print('load', neighbours_file)
                     indices = np.load(neighbours_file)
                 else:
-                    indices = indices_k_neighbours(pcs, k).astype(np.float)
+                    indices = indices_k_neighbours(pcs, k)
                     np.save(neighbours_file, indices)
                     print("Indexes file created in: ", neighbours_file)
-            pcs = np.dstack([pcs, indices])
+            pcs = np.dstack([pcs, indices.astype(np.float)])
 
         pcd.append(pcs)
         labels.append(label)
