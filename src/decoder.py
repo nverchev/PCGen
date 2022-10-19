788141d5fef357d8628b6f442186b40a985911ab
nverchev <nicolas.vercheval@gmail.com>
2022-10-19 19:21:53 +0200

    new PCGenH

--- src/decoder.py
+++ src/decoder.py
@@ -602,8 +602,8 @@ class PCGenH(nn.Module):
         correction = 1
         for group in range(self.num_groups):
             x_group = x[..., group * group_size: (group + 1) * group_size]
-            x_group = (z.unsqueeze(2) / (0.00001 + correction)) * x_group
-            correction = x_group.detach().var(2).unsqueeze(2)
+            x_group = (z.unsqueeze(2) / correction) * x_group
+            correction = torch.softmax(1 / x_group.detach().var(2), dim=1).unsqueeze(2)
             x_group = self.group_conv[group](x_group)
             xs.append(x_group)
         x = torch.cat(xs, dim=2)
