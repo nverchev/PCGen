200281517d17f3541e8bc1b225c1eb67bb5f53f4
nverchev <nicolas.vercheval@gmail.com>
2022-10-24 13:29:05 +0200

    new PCGenH and new filter

--- src/decoder.py
+++ src/decoder.py
@@ -680,55 +680,55 @@ class PCGenH(nn.Module):
         return x
 
 
-# class PCGenH(nn.Module):
-#
-#     def __init__(self, cw_dim, m, gf=True):
-#         super().__init__()
-#         self.h_dim = [256, cw_dim, 512, 512, 512, 64]
-#         self.m = m
-#         self.m_training = m
-#         self.gf = gf
-#         self.sample_dim = 16
-#         self.num_groups = 4
-#         self.map_sample1 = PointsConvLayer(self.sample_dim, self.h_dim[0], batch_norm=False, act=nn.ReLU(inplace=True))
-#         self.map_sample2 = PointsConvLayer(self.h_dim[0], self.h_dim[1], batch_norm=False, act=nn.Hardtanh(inplace=True))
-#         self.group_conv = nn.ModuleList()
-#         self.group_final = nn.ModuleList()
-#         for _ in range(self.num_groups):
-#             modules = []
-#             for in_dim, out_dim in zip(self.h_dim[1:-1], self.h_dim[2:]):
-#                 modules.append(PointsConvLayer(in_dim, out_dim, act=nn.ReLU(inplace=True)))
-#             self.group_conv.append(nn.Sequential(*modules))
-#             self.group_final.append(PointsConvLayer(self.h_dim[-1], OUT_CHAN, batch_norm=False, act=None))
-#         self.att1 = PointsConvLayer(self.h_dim[-1] * self.num_groups,  self.num_groups, act=None)
-#         self.att2 = LinearLayer(self.h_dim[-1] * self.num_groups, self.h_dim[-1] * self.num_groups, act=None)
-#
-#     def forward(self, z, s=None):
-#         batch = z.size()[0]
-#         device = z.device
-#         m = self.m_training if self.training else self.m
-#         x = s if s is not None else torch.randn(batch, self.sample_dim, m, device=device)
-#         x = x / torch.linalg.vector_norm(x, dim=1, keepdim=True)
-#         x = self.map_sample1(x)
-#         x = self.map_sample2(x)
-#         x = z.unsqueeze(2) * x
-#         xs = []
-#         for group in range(self.num_groups):
-#             x_group = self.group_conv[group](x)
-#             xs.append(x_group)
-#         x_att = torch.cat(xs, dim=1).contiguous()
-#         keys = self.att2(x_att.max(2)[0])
-#         x_att = x_att * torch.sigmoid(keys).unsqueeze(2)
-#         att = torch.softmax(self.att1(x_att), dim=1).transpose(2, 1)
-#         x_final = []
-#         for group, x_group in enumerate(xs):
-#             x_group = self.group_final[group](x_group)
-#             x_final.append(x_group)
-#         x = (torch.stack(x_final, dim=3) * att.unsqueeze(1)).sum(3)
-#
-#         if self.gf:
-#             x = graph_filtering(x)
-#         return x
+class PCGenH(nn.Module):
+
+    def __init__(self, cw_dim, m, gf=True):
+        super().__init__()
+        self.h_dim = [256, cw_dim, 512, 512, 512, 64]
+        self.m = m
+        self.m_training = m
+        self.gf = gf
+        self.sample_dim = 16
+        self.num_groups = 4
+        self.map_sample1 = PointsConvLayer(self.sample_dim, self.h_dim[0], batch_norm=False, act=nn.ReLU(inplace=True))
+        self.map_sample2 = PointsConvLayer(self.h_dim[0], self.h_dim[1], batch_norm=False, act=nn.Hardtanh(inplace=True))
+        self.group_conv = nn.ModuleList()
+        self.group_final = nn.ModuleList()
+        for _ in range(self.num_groups):
+            modules = []
+            for in_dim, out_dim in zip(self.h_dim[1:-1], self.h_dim[2:]):
+                modules.append(PointsConvLayer(in_dim, out_dim, act=nn.ReLU(inplace=True)))
+            self.group_conv.append(nn.Sequential(*modules))
+            self.group_final.append(PointsConvLayer(self.h_dim[-1], OUT_CHAN, batch_norm=False, act=None))
+        self.att1 = PointsConvLayer(self.h_dim[-1] * self.num_groups,  self.num_groups, act=None)
+        #self.att2 = LinearLayer(self.h_dim[-1] * self.num_groups, self.h_dim[-1] * self.num_groups, act=None)
+
+    def forward(self, z, s=None):
+        batch = z.size()[0]
+        device = z.device
+        m = self.m_training if self.training else self.m
+        x = s if s is not None else torch.randn(batch, self.sample_dim, m, device=device)
+        x = x / torch.linalg.vector_norm(x, dim=1, keepdim=True)
+        x = self.map_sample1(x)
+        x = self.map_sample2(x)
+        x = z.unsqueeze(2) * x
+        xs = []
+        for group in range(self.num_groups):
+            x_group = self.group_conv[group](x)
+            xs.append(x_group)
+        x_att = torch.cat(xs, dim=1).contiguous()
+        #keys = self.att2(x_att.max(2)[0])
+        #x_att = x_att * torch.sigmoid(keys).unsqueeze(2)
+        att = torch.softmax(self.att1(x_att), dim=1).transpose(2, 1)
+        x_final = []
+        for group, x_group in enumerate(xs):
+            x_group = self.group_final[group](x_group)
+            x_final.append(x_group)
+        x = (torch.stack(x_final, dim=3) * att.unsqueeze(1)).sum(3)
+
+        if self.gf:
+            x = graph_filtering(x)
+        return x
 
 def get_decoder(decoder_name):
     decoder_dict = {
