414d509cdb87ba52475247383f01e6dfcb4d4fcc
nverchev <nicolas.vercheval@gmail.com>
2022-11-05 01:51:01 +0100

    filter detach neighbours

--- src/dataset.py
+++ src/dataset.py
@@ -173,7 +173,7 @@ class Modelnet40Dataset:
         self.modelnet_path = os.path.join(data_dir, 'modelnet40_hdf5_2048')
         self.augmentation_settings = augmentation_settings
         self.download()
-        files_path = lambda x: os.path.join(modelnet_path, f'*{x}*.h5')
+        files_path = lambda x: os.path.join(self.modelnet_path, f'*{x}*.h5')
         self.pcd, self.indices, self.labels = {}, {}, {}
         for split in ['train', 'test']:
             self.pcd[split], self.indices[split], self.labels[split] = \
