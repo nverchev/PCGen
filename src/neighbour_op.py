c31548de096e0e4fad83ca386b65b0eb5b8ed664
nverchev <nicolas.vercheval@gmail.com>
2022-11-08 00:41:49 +0100

    filter detach neighbours

--- src/neighbour_op.py
+++ src/neighbour_op.py
@@ -80,5 +80,5 @@ def graph_filtering(x, k=4):
     sigma = dist[..., 0:1]
     weights = torch.softmax(-dist/sigma, dim=-1)
     weighted_neighbours = weights.unsqueeze(1).expand(-1, 3, -1, -1) * neighbours
-    x = 1.5 * x - 0.5 * weighted_neighbours.sum(-1)
+    x = 1.5 * x - 0.5 * weighted_neighbours.sum(-1).detach()
     return x
